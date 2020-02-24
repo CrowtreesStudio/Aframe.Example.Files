5f38d00b358958b10998e3164e3e695aae4109c9
Seamusboy <james@crowtreesstudio.com>
2020-02-24 15:31:24 -0500

    Updated environment change
    Keeping code local to scene.

--- A-Frame.Virtual.Reality/Aframe.Cursor.Warmup.2/README.md
+++ A-Frame.Virtual.Reality/Aframe.Cursor.Warmup.2/README.md
@@ -1,2 +1,3 @@
 ## A-frame Cursor Warm-up Example 02
-Varition on the first "warm-up" example. In this case the warm-up will change the environment implemented using the "Environment" component.
+<p>Varition on the first "warm-up" example. In this case the warm-up will change the environment implemented using the "Environment" component.</p>
+<p>Look at the 'cursor.js' file and you will see that we first set up an array with the environment presets we wish to use. Then on each click we increment the lookup number by one, progressing through the array one register at a time.</p>
--- A-Frame.Virtual.Reality/Aframe.Cursor.Warmup.2/javascript/cursor.js
+++ A-Frame.Virtual.Reality/Aframe.Cursor.Warmup.2/javascript/cursor.js
@@ -1,10 +1,14 @@
- AFRAME.registerComponent('scene-change', {
-     init: function(){
-         console.log("Hello from scene-change");
-//          var el = this.el;
-//         el.setAttribute('environment', {preset:'egypt'});
-//          console.log(el);
-     }
+// Global variable & array - bad practice really but it works
+var propertyXYZ = -1;
+var PLACES = ['japan', 'egypt', 'tron', 'forest'];
+
+AFRAME.registerComponent('scenechange', {
+    places: function(){
+        propertyXYZ++;// increment by one        
+        propertyXYZ = propertyXYZ % PLACES.length;
+        let newPlace = PLACES[propertyXYZ];
+        return newPlace;
+    }
  });
 
 
@@ -14,18 +18,19 @@ AFRAME.registerComponent('cursor-listener', {
         let env = document.getElementById('scene');
         console.log(env);
         var lastIndex = -1;
-        var lastPlaceIndex = -1;
+        var lastPlaceIndex = 0;
         var COLORS = ['purple', 'green', 'blue'];
-        var PLACES = ['japan', 'egypt', 'tron', 'forest'];
         var message = "Now it's ";
+        
         this.el.addEventListener('click', function (evt) {
+            // colour change
             lastIndex = (lastIndex + 1) % COLORS.length;
             this.setAttribute('material', 'color', COLORS[lastIndex]);
-            console.log('I was clicked at: ', evt.detail.intersection.point);
             document.getElementById("text").setAttribute("value", message+COLORS[lastIndex]);
             
-            lastPlaceIndex = (lastPlaceIndex + 1) % PLACES.length;
-            env.setAttribute('environment', {preset:PLACES[lastPlaceIndex]});
+            // Change the scene        
+            let change = document.querySelector('[scenechange]').components.scenechange.places();
+            env.setAttribute('environment', {preset:change});
             
         });
     }
